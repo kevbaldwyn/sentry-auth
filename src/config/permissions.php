cb313c2a3892d5c09909f0c964cee83b073ab264
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2013-05-26 19:34:17 +0100

    Added permissioms config

---
+++ src/config/permissions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * A list of available permissions
+ */	
+ 
+return array(
+
+	'admin' => array(
+
+		'users' => array(
+
+			'edit', 
+			'create', 
+			'delete'
+		
+		),
+
+		'groups' => array(
+
+			'edit', 
+			'create', 
+			'delete',
+			'add_users'
+		
+		)
+
+	)
+
+);
